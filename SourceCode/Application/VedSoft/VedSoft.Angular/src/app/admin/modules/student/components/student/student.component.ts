d0532e037e60e3f327c241964971536f78275fba
guruvenamah2019 <52539995+guruvenamah2019@users.noreply.github.com>
2019-09-15 00:10:23 +0530

    Ram- prod build

--- SourceCode/Application/VedSoft/VedSoft.Angular/src/app/admin/modules/student/components/student/student.component.ts
+++ SourceCode/Application/VedSoft/VedSoft.Angular/src/app/admin/modules/student/components/student/student.component.ts
@@ -1,27 +1,101 @@
 ﻿import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 import { BankService, AuthenticationService } from 'src/app/core/services';
+import { Router, ActivatedRoute } from '@angular/router';
+import { MatTabChangeEvent } from '@angular/material/tabs';
 
 
 @Component({
   templateUrl: 'student.component.html',
 })
 
+
+
 export class StudentComponent implements OnInit {
+  navLinks: any[];
+  activeLinkIndex = -1;
 
   loading = false;
   error = '';
   submitted: boolean = false;
   onNavigate() {
   }
-  constructor(private userService: AuthenticationService) {
+  constructor(private userService: AuthenticationService, private router: Router,private activatedRoute: ActivatedRoute) {
     console.log("StudentComponent");
+
+    this.navLinks = [
+      {
+          label: 'Profile',
+          link: './profile',
+          index: 0
+      }, {
+          label: 'Enquiry',
+          link: './enquiry',
+          index: 1
+      }, {
+          label: 'Admission',
+          link: './admission',
+          index: 2
+      }, 
+      {
+        label: 'Performance',
+        link: './performance',
+        index: 3
+    }, 
+    {
+      label: 'Attendance',
+      link: './attendance',
+      index: 4
+  }, 
+  {
+    label: 'Punches',
+    link: './punches',
+    index: 5
+}, 
+{
+  label: 'Batch',
+  link: './batch',
+  index: 6
+}, 
+{
+  label: 'Academic',
+  link: './academic',
+  index: 7
+}, 
+{
+  label: 'Leaves',
+  link: './leaves',
+  index: 8
+},
+{
+  label: 'Login',
+  link: './login',
+  index: 9
+},
+{
+  label: 'Documents',
+  link: './documents',
+  index: 10
+},
+{
+  label: 'Assignments',
+  link: './assignments',
+  index: 11
+}, 
+  ];
 
   }
   ngOnInit() {
+    this.router.events.subscribe((res) => {
+      this.activeLinkIndex = this.navLinks.indexOf(this.navLinks.find(tab => tab.link === '.' + this.router.url));
+  });
 
 
   }
+  navigate(event: MatTabChangeEvent) {
+    const tabData = this.navLinks[event.index];    
+   this.router.navigate([tabData.link], { relativeTo: this.activatedRoute });
+}
   
 
 }
--- SourceCode/Application/VedSoft/VedSoft.Angular/src/app/admin/modules/student/student-route.ts
+++ SourceCode/Application/VedSoft/VedSoft.Angular/src/app/admin/modules/student/student-route.ts
@@ -1,10 +1,51 @@
 ﻿import { Routes } from '@angular/router';
-import { StudentIndexComponent, StudentListComponent, StudentComponent } from './components';
+import { StudentIndexComponent, StudentListComponent, StudentComponent, StudentProfileComponent, StudentEnquiryComponent, StudentAdmissionComponent, StudentAssignmentsComponent, StudentDocumentsComponent, StudentLoginComponent, StudentLeavesComponent, StudentAcademicComponent, StudentBatchComponent, StudentPunchesComponent, StudentAttendanceComponent } from './components';
 export const STUDENT_ROUTES: Routes = [
     { 
         path: '', component: StudentListComponent},
            
-            { path: 'student/:id', component: StudentComponent}
+            { path: 'student/:id', component: StudentComponent,children: [
+                {
+                    path: '', component: StudentProfileComponent
+                  },
+      
+                {
+                  path: 'profile', component: StudentProfileComponent
+                },
+                {
+                  path: 'enquiry', component: StudentEnquiryComponent
+                },
+                {
+                    path: 'admission', component: StudentAdmissionComponent
+                  },
+                  {
+                    path: 'assignments', component: StudentAssignmentsComponent
+                  },
+                  {
+                    path: 'documents', component: StudentDocumentsComponent
+                  },
+                  {
+                    path: 'login', component: StudentLoginComponent 
+                  },
+                  {
+                    path: 'leaves', component: StudentLeavesComponent
+                  },
+                  {
+                    path: 'academic', component: StudentAcademicComponent
+                  },
+                  {
+                    path: 'batch', component: StudentBatchComponent
+                  },
+                  {
+                    path: 'punches', component: StudentPunchesComponent
+                  },
+                  {
+                    path: 'attendance', component: StudentAttendanceComponent
+                  },
+                 
+              ]
+        
+        }
     
 ];
 
